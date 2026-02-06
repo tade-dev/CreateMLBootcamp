ee45b60bcc6d74ba9b9c71456ff55e3888fab2b4
Akintade Oluwaseun <akintadeseun816@gmail.com>
2026-02-06 20:57:43 +0000

    Initial Commit

---
+++ CreateMLBootcamp/CreateMLBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateMLBootcampApp.swift
+//  CreateMLBootcamp
+//
+//  Created by BSTAR on 06/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CreateMLBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
